4bf0bfc6a25424b6d57bd0730e976a0b757fe889
Saul Hernandez <sauldhernandez@gmail.com>
2015-03-30 16:12:29 -0430

    Fixed bug for handler injection

--- dist/sgb-screen-login.js
+++ dist/sgb-screen-login.js
@@ -12,7 +12,7 @@ angular.module('sgb-screen-login', ['megazord'])
 
         var loginHandler;
         if(_screenParams.loginHandler) {
-            loginHandler = $stateParams.loginHandler;
+            loginHandler = _screenParams.loginHandler;
         }
         else {
             loginHandler = defaultLoginHandler;
