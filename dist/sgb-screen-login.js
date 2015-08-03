2d1771ecfcfc49bf604c9ff73ebd0db3a67e87b5
luismiglietti <luismiglietti@gmail.com>
2015-08-03 16:37:57 -0430

    added language

--- dist/sgb-screen-login.js
+++ dist/sgb-screen-login.js
@@ -1,16 +1,19 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
-
 angular.module('sgb-screen-login', ['megazord'])
     .controller('sgb-screen-login-controller', ['_router', '_screenParams', '_screen', '$injector', '$stateParams', '$scope', '$translate', '$q', function(_router, _screenParams, _screen, $injector, $stateParams, $scope, $translate, $q){
 
         _screen.initialize($scope, _screenParams);
 
+        //Screen template parameters
         $scope.companyLogo = _screenParams.companyLogo;
         $scope.userPassFields = _screenParams.userPassFields;
         $scope.clearSubmitButtons = _screenParams.clearSubmitButtons;
         $scope.enterButton = _screenParams.enterButton;
+
+        //Dummy implementation for blocked account
+        $scope.missingAttempts = 0; 
 
         var defaultLoginHandler = function(username, password) {
             //TODO: Default to rest api call instead of this dummy implementation
@@ -41,38 +44,44 @@ angular.module('sgb-screen-login', ['megazord'])
         };
 
         $scope.doLogin = function() {
-            //Validate username and password (if needed)
-            console.log('username val is ' + _screenParams.usernameValidation);
-            if(_screenParams.usernameValidation) {
-                var exp = new RegExp(_screenParams.usernameValidation);
-                if(!exp.test($scope.login.username)) {
-                    alert($translate('login_invalid_username'));
-                    return;
-                }
-            }
 
-            if(_screenParams.passwordValidation) {
-                var exp = new RegExp(_screenParams.passwordValidation);
-                if(!exp.test($scope.login.password)) {
-                    alert($translate('login_invalid_password'));
-                    return;
+            if ($scope.missingAttempts>0) {
+                //Validate username and password (if needed)
+                console.log('username val is ' + _screenParams.usernameValidation);
+                if(_screenParams.usernameValidation) {
+                    var exp = new RegExp(_screenParams.usernameValidation);
+                    if(!exp.test($scope.login.username)) {
+                        alert($translate('login_invalid_username'));
+                        return;
+                    }
                 }
-            }
 
-            $injector.invoke(loginHandler, null, { username: $scope.login.username, password: $scope.login.password })
-                .then(function(result){
-                    if(result) {
-                        _router.fireEvent({name: 'loginFinished', params: {
-                            username: $scope.login.username,
-                            password: $scope.login.password
-                        }});
+                if(_screenParams.passwordValidation) {
+                    var exp = new RegExp(_screenParams.passwordValidation);
+                    if(!exp.test($scope.login.password)) {
+                        alert($translate('login_invalid_password'));
+                        return;
                     }
-                    else {
-                        $translate('login_invalid_credentials').then(function(message){
-                            alert(message);
-                        });
-                    }
-                });
+                }
+
+                $injector.invoke(loginHandler, null, { username: $scope.login.username, password: $scope.login.password })
+                    .then(function(result){
+                        if(result) {
+                            _router.fireEvent({name: 'loginFinished', params: {
+                                username: $scope.login.username,
+                                password: $scope.login.password
+                            }});
+                        }
+                        else {
+                            $translate('login_invalid_credentials').then(function(message){
+                                $scope.missingAttempts--; 
+                                alert($scope.missingAttempts?message:'Su cuenta se ha bloqueado');
+                            });
+                        }
+                    });
+            } else {
+                alert('Su cuenta se encuentra bloqueada');
+            }
         };
     }]);
 
